d7266ac3b64559413d927bbe213b569d93cbf511
Barak Barzilay <barakoyb@gmail.com>
2021-09-21 20:21:57 +0300

    feat(popup-form): add popup form to edit profile via edit button
    add popup form
    can now alter name and profession

---
+++ pages/script.js
@@ -0,0 +1,31 @@
+let profileContainer = document.querySelector('.profile');
+let profileName = profileContainer.querySelector('.profile__name');
+let profileProfession = profileContainer.querySelector('.profile__about');
+let profileEditButton = profileContainer.querySelector('.profile__edit-btn');
+
+let popupBox = document.querySelector('.popup');
+let closeForm = popupBox.querySelector('.popup__close-btn');
+
+let formElement = popupBox.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__name');
+let professionInput = formElement.querySelector('.popup__profession');
+
+let submitForm = formElement.querySelector('.popup__submit-btn');
+
+function toggleForm() {
+  nameInput.value = profileName.textContent;
+  professionInput.value = profileProfession.textContent;
+  popupBox.classList.toggle('popup_visible');
+}
+
+profileEditButton.addEventListener('click', toggleForm);
+closeForm.addEventListener('click', toggleForm);
+
+function formHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileProfession.textContent = professionInput.value;
+  popupBox.classList.toggle('popup_visible');
+}
+
+submitForm.addEventListener('click', formHandler);
