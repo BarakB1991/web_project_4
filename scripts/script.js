a7b0e50bc6162835acbc19df274d6436cdef6e06
BarakBarzilay <barakoyb@gmail.com>
2021-10-05 08:17:21 +0300

    add initialize card function with JS template added to MOD

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,52 @@
+// initialize cards on page
+
+const initialCards = [
+  {
+    name: 'Yosemite Valley',
+    link: 'https://code.s3.yandex.net/web-code/yosemite.jpg'
+  },
+  {
+    name: 'Lake Louise',
+    link: 'https://code.s3.yandex.net/web-code/lake-louise.jpg'
+  },
+  {
+    name: 'Bald Mountains',
+    link: 'https://code.s3.yandex.net/web-code/bald-mountains.jpg'
+  },
+  {
+    name: 'Latemar',
+    link: 'https://code.s3.yandex.net/web-code/latemar.jpg'
+  },
+  {
+    name: 'Vanoise National Park',
+    link: 'https://code.s3.yandex.net/web-code/vanoise.jpg'
+  },
+  {
+    name: 'Lago di Braies',
+    link: 'https://code.s3.yandex.net/web-code/lago.jpg'
+  }
+];
+
+function createCard(card) {
+  // grabbing template and copying .card node
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const cardTitleElement = cardElement.querySelector('.card__title');
+  const cardLinkElement = cardElement.querySelector('.card__img');
+  cardTitleElement.textContent = card.name;
+  cardLinkElement.src = card.link;
+  return cardElement;
+}
+
+const cardsContainer = document.querySelector('.cards__list');
+
+initialCards.forEach(card => {
+  const cardElement = createCard(card);
+  cardsContainer.append(cardElement);
+});
+
+// popup
 let profileContainer = document.querySelector('.profile');
 let profileName = profileContainer.querySelector('.profile__name');
 let profileProfession = profileContainer.querySelector('.profile__about');
