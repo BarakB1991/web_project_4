a573f0afd6e54c3f313f75e90cbb36935633ed7d
BarakBarzilay <barakoyb@gmail.com>
2021-10-20 09:13:10 +0300

    refactor code to function oriented. add features: 1. close popup via exc and outside click.

--- scripts/script.js
+++ scripts/script.js
@@ -111,10 +111,36 @@ function submitEditProfileForm(evt) {
 
 function openPopup(popupBox) {
   popupBox.classList.add('popup_visible');
+  setClosePopupEventListeners();
 }
 
 function closePopup(popupBox) {
   popupBox.classList.remove('popup_visible');
+  removeClosePopupEventListeners();
+}
+
+function setClosePopupEventListeners() {
+  document.addEventListener('keydown', closePopupViaEscButton);
+  document.addEventListener('mousedown', closePopupViaOutsideClick);
+  // mousedown is most suited for when clicking inside input and dragging mouse to mark all text (before deletion)
+}
+
+function removeClosePopupEventListeners() {
+  document.removeEventListener('keydown', closePopupViaEscButton);
+  document.removeEventListener('mousedown', closePopupViaOutsideClick);
+}
+
+function closePopupViaOutsideClick(evt) {
+  console.log(evt.target);
+  if (evt.target.classList.contains(`popup`)) {
+    closePopup(evt.target);
+  }
+}
+
+function closePopupViaEscButton(evt) {
+  if (evt.key === 'Escape') {
+    closePopup(document.querySelector('.popup_visible'));
+  }
 }
 
 function matchProfileValues() {
@@ -142,9 +168,9 @@ profileEditButton.addEventListener('click', () => {
 });
 
 profileAddCardFormButton.addEventListener('click', () => {
-  openPopup(addCardPopup);
   resetNewCardForm();
   checkInitialFormValidity(cardAddForm, pageSettings);
+  openPopup(addCardPopup);
 });
 
 // Submit handlers
