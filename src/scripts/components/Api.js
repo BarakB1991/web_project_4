c7c1cbc55fb19e6f9a62ec200e34b9ca49e911e7
Barak Barzilay <barakoyb@gmail.com>
2022-03-18 15:23:02 +0200

    some work done on class Api and index.js calling new Api

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -1,35 +1,20 @@
 export default class Api {
   constructor(options) {
-    this.baseUrl = options.baseUrl;
-    this.headers = options.headers;
+    this._url = options.baseUrl;
+    this._token = options.token;
   }
 
-  getInitialCards = () => {
-    fetch('https://around.nomoreparties.co/v1/group-12/cards', {
-      headers: {
-        authorization: 'c785e696-84a9-4aca-b3d2-750b2694b444'
-      }
-    })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
+  getInitialCards() {
+    return fetch(`${this._url}/cards`, {
+      headers: {authorization: this._token}
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
         return Promise.reject(`Error: ${res.status}`);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    // fetch(`${this.baseUrl}/cards`, this.headers)
-    //   .then(res => {
-    //     if (res.ok) {
-    //       return res.json();
-    //     }
-    //     return Promise.reject(`Error: ${res.status}`);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
-  };
+      }
+    });
+  }
 
   getUserData = () => {};
 
--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,87 +1,95 @@
-import '../../pages/index.css';
-import FormValidator from '../components/FormValidator.js';
-import Card from '../components/Card.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage';
-import PopupWithForm from '../components/PopupWithForm';
+// import '../../pages/index.css';
+// import FormValidator from '../components/FormValidator.js';
+// import Card from '../components/Card.js';
+// import Section from '../components/Section.js';
+// import PopupWithImage from '../components/PopupWithImage';
+// import PopupWithForm from '../components/PopupWithForm';
 
-import {
-  initialCards,
-  editProfilePopup,
-  addCardPopup,
-  professionInput,
-  nameInput,
-  profileName,
-  profileProfession,
-  profileEditButton,
-  profileAddCardFormButton,
-  cardsContainer,
-  cardTemplateSelector
-} from '../utils/constants.js';
-import UserInfo from '../components/UserInfo.js';
+// import {
+//   editProfilePopup,
+//   addCardPopup,
+//   professionInput,
+//   nameInput,
+//   profileName,
+//   profileProfession,
+//   profileEditButton,
+//   profileAddCardFormButton,
+//   // initialCards,
+//   cardTemplateSelector
+// } from '../utils/constants.js';
+// import UserInfo from '../components/UserInfo.js';
 import Api from '../components/Api.js';
 
 const api = new Api({
   baseUrl: 'https://around.nomoreparties.co/v1/group-12',
-  headers: {
-    authorization: 'c785e696-84a9-4aca-b3d2-750b2694b444',
-    'Content-Type': 'application/json'
-  }
+  token: 'c785e696-84a9-4aca-b3d2-750b2694b444'
 });
 
-const userInfo = new UserInfo({name: profileName, profession: profileProfession});
-userInfo.setUserInfo({name: 'Abraham Lincoln', profession: 'A founding Father'});
+// const userInfo = new UserInfo({name: profileName, profession: profileProfession});
+// userInfo.setUserInfo({name: 'Abraham Lincoln', profession: 'A founding Father'});
 
-const imagePopupWindow = new PopupWithImage('.popup_type_image');
-// Initialize  cards
+// const imagePopupWindow = new PopupWithImage('.popup_type_image');
+// // Initialize  cards
 
-function renderCard(item) {
-  const card = new Card(item, cardTemplateSelector, imagePopupWindow.open);
-  const cardElement = card.renderCard();
-  cardSection.addItem(cardElement);
-}
+// function renderCard(item) {
+//   const card = new Card(item, cardTemplateSelector, imagePopupWindow.open);
+//   const cardElement = card.renderCard();
+//   cardSection.addItem(cardElement);
+// }
 
-const cardSection = new Section('.cards__list', {
-  items: api.getInitialCards(),
-  renderer: renderCard
-});
-cardSection.renderer();
+const initialCards = api
+  .getInitialCards()
+  .then(cards => {
+    return cards;
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-const profilePopupWindow = new PopupWithForm('.popup_type_edit-profile', data => {
-  userInfo.setUserInfo(data);
-});
+console.log('cards: ' + typeof initialCards + initialCards);
 
-const addCardPopupWindow = new PopupWithForm('.popup_type_add-card', data => {
-  const {cardtitle: name, imagelink: link} = data;
-  renderCard({name, link});
-});
+// const cardSection = new Section('.cards__list', {
+//   items: initialCards,
+//   renderer: renderCard
+// });
 
-// imagePopupWindow.setEventListeners();
-profilePopupWindow.setEventListeners();
-addCardPopupWindow.setEventListeners();
+// cardSection.renderer();
 
-const formSettings = {
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-btn',
-  inactiveButtonClass: 'popup__submit-btn_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-};
+// const profilePopupWindow = new PopupWithForm('.popup_type_edit-profile', data => {
+//   userInfo.setUserInfo(data);
+// });
 
-const cardFormValidator = new FormValidator(formSettings, addCardPopup);
-const profileFormValidator = new FormValidator(formSettings, editProfilePopup);
+// const addCardPopupWindow = new PopupWithForm('.popup_type_add-card', data => {
+//   const {cardtitle: name, imagelink: link} = data;
+//   renderCard({name, link});
+// });
 
-profileEditButton.addEventListener('click', () => {
-  profilePopupWindow.open();
-  const {name, profession} = userInfo.getUserInfo(); // get object of current name and profession
-  nameInput.value = name;
-  professionInput.value = profession;
-});
+// // imagePopupWindow.setEventListeners();
+// profilePopupWindow.setEventListeners();
+// addCardPopupWindow.setEventListeners();
 
-profileAddCardFormButton.addEventListener('click', () => {
-  addCardPopupWindow.open();
-  cardFormValidator.resetValidation();
-});
+// const formSettings = {
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__submit-btn',
+//   inactiveButtonClass: 'popup__submit-btn_disabled',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__error_visible'
+// };
 
-cardFormValidator.enableValidation();
-profileFormValidator.enableValidation();
+// const cardFormValidator = new FormValidator(formSettings, addCardPopup);
+// const profileFormValidator = new FormValidator(formSettings, editProfilePopup);
+
+// profileEditButton.addEventListener('click', () => {
+//   profilePopupWindow.open();
+//   const {name, profession} = userInfo.getUserInfo(); // get object of current name and profession
+//   nameInput.value = name;
+//   professionInput.value = profession;
+// });
+
+// profileAddCardFormButton.addEventListener('click', () => {
+//   addCardPopupWindow.open();
+//   cardFormValidator.resetValidation();
+// });
+
+// cardFormValidator.enableValidation();
+// profileFormValidator.enableValidation();
